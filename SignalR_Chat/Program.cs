555e6a465ea0f859406f48d2867471a032a01282
İsmail Baran <ismailbaran04@gmail.com>
2024-02-27 13:23:13 +0300

    signal r eklendi

--- SignalR_Chat/Program.cs
+++ SignalR_Chat/Program.cs
@@ -1,8 +1,10 @@
+using SignalR_Chat.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,5 +25,5 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapHub<ChatHub>("/chatHub");
 app.Run();
