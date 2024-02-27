70892958c73eb9120106c3fe62e49c92e24c68a4
İsmail Baran <ismailbaran04@gmail.com>
2024-02-27 13:22:38 +0300

    kütüphaneler eklendi

--- SignalR_Chat/obj/Debug/net8.0/SignalR_Chat.AssemblyInfo.cs
+++ SignalR_Chat/obj/Debug/net8.0/SignalR_Chat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalR_Chat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2238cb60572e47912d812d4d6a484903b1dc29e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalR_Chat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalR_Chat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
